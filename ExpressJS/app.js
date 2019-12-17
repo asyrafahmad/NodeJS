f9ec63ff0b1de276a2f108384e38bf9059f01cf4
asyrafahmad <are.syraf9764@gmail.com>
2019-12-17 12:41:56 +0800

    Express router (add)

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,19 +3,12 @@ const bodyParser = require('body-parser');          //import body parser
 
 const app = express();                              //execute expressJS (initialize object)
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(express.urlencoded({extended: true}));                   //call body parser
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');        //send = to display
-});                                          
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {           
-    res.send('<h1>Hello from expressJS</h1>');
-});                                          
+app.use(adminRoutes());
+app.use(shopRoutes());  
 
 app.listen(3000);
