5e1d738cc97f40b14cefcbebae7d6ed0e4fd3bc8
asyrafahmad <are.syraf9764@gmail.com>
2019-12-17 12:12:19 +0800

    Parsing incoming request (add)

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,19 +1,20 @@
 const express = require('express');                 //import expressJS
+const bodyParser = require('body-parser');          //import body parser
 
 const app = express();                              //execute expressJS (initialize object)
 
-app.use('/', (req, res, next) => {
-    console.log('This is always runs!');
-    next();
-});
+app.use(express.urlencoded({extended: true}));                   //call body parser
 
-app.use('/add-product', (req, res, next) => {                        
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');        //send = to display
 });                                          
 
-app.use('/', (req, res, next) => {                        
-    console.log('In another middleware!');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {           
     res.send('<h1>Hello from expressJS</h1>');
 });                                          
 
