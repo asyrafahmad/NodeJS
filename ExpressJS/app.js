e9d7d05432c4ae1f097e426f032b4345d1911404
asyrafahmad <are.syraf9764@gmail.com>
2019-12-30 15:52:41 +0800

    Serving Files Staticcally (add public file)

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -9,13 +9,14 @@ const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(express.urlencoded({extended: true}));                                  //call body parser
+app.use(express.static(path.join(__dirname, 'public')));                        //set path for public folder
 
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);  
 
 //to display 404 not found
 app.use((req, res, next) => {
-    res.status(404).sendFiles(path.join(__dirname, 'views', '404.html'));       //status(404) = to response 404 not found
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));       //status(404) = to response 404 not found
 });
 
 app.listen(3000);
