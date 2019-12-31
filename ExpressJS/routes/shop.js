75460bd0dce8844c318cca47263d36580d43fcc8
asyrafahmad <are.syraf9764@gmail.com>
2019-12-31 15:06:40 +0800

    Outputting dynamic content

--- ExpressJS/routes/shop.js
+++ ExpressJS/routes/shop.js
@@ -8,7 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop');                                                 //return the default templating engine                                     
+    const product = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});                                                 //return the default templating engine                                     
 });                                          
 
-module.exports = router;  a
+module.exports = router; 
